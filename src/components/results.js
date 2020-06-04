a7fd3902b777c67e34ee248350cafa878bf706f6
Junichi Yokota <jun68.ykt@gmail.com>
2020-06-04 17:43:03 +0900

    Fix prop "key" to avoid duplication

--- src/components/results.js
+++ src/components/results.js
@@ -12,9 +12,9 @@ class Results extends React.Component {
       );
     } else {
       return(
-        this.props.arrayExample.map(arr => {
+        this.props.arrayExample.map((arr, i) => {
           return (
-            <table key={`table-${arr.id}`}>
+            <table key={`table-${i}`}>
               <tbody>
               <tr><td>{arr.id}</td></tr>
               <tr><td>{arr.testName}</td></tr>
